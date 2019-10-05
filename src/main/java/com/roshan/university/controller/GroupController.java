e598d4a71e5cbdbc07ff4ff173bff7bf33bf1f44
Roshan Priyadarshana <roshan.p@aeturnum.com>
2019-10-06 01:13:54 +0530

    add group

--- src/main/java/com/roshan/university/controller/GroupController.java
+++ src/main/java/com/roshan/university/controller/GroupController.java
@@ -2,13 +2,13 @@ package com.roshan.university.controller;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,7 +17,9 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.roshan.university.model.Course;
 import com.roshan.university.model.Group;
+import com.roshan.university.repository.CourseRepository;
 import com.roshan.university.repository.GroupRepository;
 
 @Controller
@@ -26,6 +28,8 @@ public class GroupController {
     private Logger log = LoggerFactory.getLogger(GroupController.class);
 
     private @Autowired GroupRepository groupRepository;
+
+    private @Autowired CourseRepository courseRepository;
 
     @GetMapping("/groups/result")
     public String result(Model model) {
@@ -41,8 +45,11 @@ public class GroupController {
     }
 
     @GetMapping("/groups")
-    public String index(Group group) {
+    public String index(Group group, Model model) {
         this.log.info("Loading group form {}.", group);
+
+        List<Course> courses = this.courseRepository.findAll();
+        model.addAttribute("courses", courses);
         return "group/index";
     }
 
@@ -54,6 +61,20 @@ public class GroupController {
             if (bindingResult.hasErrors()) {
 
                 this.log.info("Form has some errors");
+                List<Course> courses = this.courseRepository.findAll();
+                model.addAttribute("courses", courses);
+                return "group/index";
+            }
+
+            Optional<Group> groupOptional = this.groupRepository.findByName(group.getName());
+            if (groupOptional.isPresent()) {
+                this.log.error("Group with name \"{}\" is already exists.", group.getName());
+                ObjectError error = new FieldError("group", "name",
+                        "Group with name \"" + group.getName() + "\" is already exists.");
+
+                bindingResult.addError(error);
+                List<Course> courses = this.courseRepository.findAll();
+                model.addAttribute("courses", courses);
                 return "group/index";
             }
 
@@ -69,17 +90,20 @@ public class GroupController {
             model.addAttribute("groups", groups);
 
             return "group/result";
+        } catch (NumberFormatException e) {
+            this.log.error(e.getMessage(), e);
+            ObjectError error = new FieldError("group", "capacity", "Please provide a valid number for Capacity");
+
+            bindingResult.addError(error);
+            List<Course> courses = this.courseRepository.findAll();
+            model.addAttribute("courses", courses);
+            return "group/index";
         } catch (Exception e) {
             this.log.error(e.getMessage(), e);
             ObjectError error = new FieldError("group", "name", "Group cannot be saved.");
-            if (e instanceof DataIntegrityViolationException) {
-                error = new FieldError("group", "name",
-                        "Group with name \"" + group.getName() + "\" is already exists.");
-            } else if (e instanceof NumberFormatException) {
-                error = new FieldError("group", "capacity", "Please provide a valid number for Capacity");
-            }
             bindingResult.addError(error);
-
+            List<Course> courses = this.courseRepository.findAll();
+            model.addAttribute("courses", courses);
             return "group/index";
         }
     }
--- src/main/java/com/roshan/university/model/Group.java
+++ src/main/java/com/roshan/university/model/Group.java
@@ -2,17 +2,25 @@ package com.roshan.university.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 @Entity
 @Table(name = "groupe")
-public class Group {
+public class Group extends AuditModel {
+
+    private static final long serialVersionUID = 277082093203621205L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,6 +34,12 @@ public class Group {
     @Positive(message = "Please provide a valid number")
     @NotNull(message = "Please provide a capacity")
     private Integer capacity;
+
+    @NotNull(message = "Course cannot be empty")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "course_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Course course;
 
     public Long getId() {
         return this.id;
@@ -49,6 +63,20 @@ public class Group {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Course getCourse() {
+        return this.course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "Group [id=" + this.id + ", name=" + this.name + ", capacity=" + this.capacity + ", course="
+                + this.course + "]";
     }
 
 }
